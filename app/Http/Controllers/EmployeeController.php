45316338d1ba00d79fb5b1c18d382ae3a67bdafa
Bilal Ahmad <bilalmujahid89@gmail.com>
2022-09-23 01:14:57 +0500

    23-09-2022 from home pc

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Employee;
+use App\Legalheir;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -14,7 +15,7 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
